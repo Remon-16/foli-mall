30716808706c04b2cb0586cbd26eb95cd1834924
REMON\Remon <982581058@qq.com>
2026-06-24 16:46:55 +0800

    初始化项目

---
+++ foli-backend/src/main/java/com/github/foli_backend/FoliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.foli_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoliBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoliBackendApplication.class, args);
+	}
+
+}
---
+++ foli-backend/src/test/java/com/github/foli_backend/FoliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.foli_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoliBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
